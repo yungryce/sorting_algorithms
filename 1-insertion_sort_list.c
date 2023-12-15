6d1fbb6a4f5fce04aa8cf77d60fcbd3a09f5d0e5
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-15 14:47:41 +0100

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = (*list)->next;
 	listint_t *temp, *temp_prev, *temp_next;
 
-	if (!list || !(*list) || !((*list)->next))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (current)
