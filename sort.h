36c32bf0882e73014ecb0439a34654bffca0f71f
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-15 18:29:14 +0100

    Counting sort

--- sort.h
+++ sort.h
@@ -41,17 +41,22 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 /* insertion sort */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
-
-/* cocktail sort */
-void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
-void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 
 /* quick sort */
 void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
+
+/* cocktail sort */
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+
+/* counting sort */
+int get_max(int *array, int size);
+
 
 #endif
