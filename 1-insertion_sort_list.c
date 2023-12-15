4ee7c681d5bdc4f67c6cad0899fc8c3d7daa1531
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-15 18:16:29 +0100

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,45 +1,49 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts DLL in ascending order using the Insertion sort
- * @list: list to be sorted
- *
- * Return: void
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
-	listint_t *temp, *temp_prev, *temp_next;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (current)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		temp = current;
-		while (temp->prev && temp->n < temp->prev->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			temp_prev = temp->prev;
-			temp_next = temp->next;
-
-			temp->prev = temp_prev->prev;
-			temp->next = temp_prev;
-
-			if (temp_prev->prev)
-				temp_prev->prev->next = temp;
-			temp_prev->prev = temp;
-
-			if (temp_next)
-				temp_next->prev = temp_prev;
-
-			temp_prev->next = temp_next;
-
-			if (!temp->prev)
-				*list = temp;
-
-			print_list(*list);
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
+
