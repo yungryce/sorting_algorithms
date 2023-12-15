adee9dcb604074b4a16b1ef319ac7f57eb4a8cdd
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-15 14:35:04 +0100

    setup

--- sort.h
+++ sort.h
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,9 +40,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+
+
 void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void shell_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
